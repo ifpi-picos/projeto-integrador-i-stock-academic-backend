53ed14dcdf4d0f8e97f5f0e9e4834412b329a122
Janderson <jandersonbrito72@gmail.com>
2022-01-31 22:26:14 -0300

    fuctions return transactions and balance

--- src/database/migrations/20211103234623-transactions.js
+++ src/database/migrations/20211103234623-transactions.js
@@ -1,66 +1,66 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    return await queryInterface.createTable('transactions', {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false
-      },
-      type_operation: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      type_material: {
-        type: Sequelize.STRING,
-        allowNull: true
-      },
-      price_kg: {
-        type: Sequelize.DOUBLE,
-        allowNull: true
-      },
-      kgs: {
-        type: Sequelize.DOUBLE,
-        allowNull: true
-      },
-      type_payment: {
-        type: Sequelize.STRING,
-        allowNull: true
-      },
-      responsible_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'users',
-          key: 'id'
-        },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE'
-      },
-      wallet_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'wallet',
-          key: 'id'
-        },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE'
-      },
-      created_at: {
-        type: Sequelize.DATE,
-        allowNull: false
-      },
-      updated_at: {
-        type: Sequelize.DATE,
-        allowNull: false
-      }
-    })
-  },
+    up: async (queryInterface, Sequelize) => {
+        return await queryInterface.createTable('transactions', {
+            id: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false
+            },
+            type_operation: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            type_material: {
+                type: Sequelize.STRING,
+                allowNull: true
+            },
+            price_kg: {
+                type: Sequelize.DOUBLE,
+                allowNull: true
+            },
+            kgs: {
+                type: Sequelize.DOUBLE,
+                allowNull: true
+            },
+            type_payment: {
+                type: Sequelize.STRING,
+                allowNull: true
+            },
+            responsible_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: {
+                    model: 'users',
+                    key: 'id'
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE'
+            },
+            wallet_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: {
+                    model: 'wallet',
+                    key: 'id'
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE'
+            },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false
+            }
+        })
+    },
 
-  down: async (queryInterface, Sequelize) => {
-    return await queryInterface.dropTable('transactions')
-  }
+    down: async (queryInterface, Sequelize) => {
+        return await queryInterface.dropTable('transactions')
+    }
 };
--- src/routes/transactions.routes.js
+++ src/routes/transactions.routes.js
@@ -3,6 +3,8 @@ const TransactionController = require('../controllers/TransactionController')
 
 const router = Router()
 
+router.get('/transactions/all', TransactionController.getTransactions)
+
 router.post('/transactions', TransactionController.saveTransaction)
 
 module.exports = router
